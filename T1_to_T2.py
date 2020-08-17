0f7a47fbdb333ad238e94152094cd96001986156
Yongxin Zhou <zhou.yongxin@hinacom.com>
2020-08-17 21:20:31 +0800

    开发中...................

--- T1_to_T2.py
+++ T1_to_T2.py
@@ -1,4 +1,4 @@
-
+# %%
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
@@ -14,40 +14,26 @@ import time
 from matplotlib import pyplot as plt
 from IPython import display
 
+# %%
 # 指定CPU计算
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-
 # %%
-
-# !wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YXtAT17LqyRm0_lQ7KN0rVvmekzh6sc5' -O ./T1-and-T2.zip
-# !unzip -q ./T1-and-T2.zip
-PATH = './T1_T2_mri_100/'
-
-BUFFER_SIZE = 400
-BATCH_SIZE = 1
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+PATH = 'e:\\T1T2'
+T1_PATH = os.path.join(PATH, 'T1')
+T2_PATH = os.path.join(PATH, 'T2')
 
 # %%
 # Cell 4
 
 def load(image_file):
   image = tf.io.read_file(image_file)
-  image = tf.image.decode_png(image, channels=3)
+  image = tf.image.decode_bmp(image, channels=3)
 
-  w = tf.shape(image)[1]
+  float_image = tf.cast(image, tf.float32)
 
-  w = w // 2
-  real_image = image[:, :w, :]
-  input_image = image[:, w:, :]
-
-  input_image = tf.cast(input_image, tf.float32)
-  real_image = tf.cast(real_image, tf.float32)
-
-  return input_image, real_image
-
+  return float_image  
 
 # %%
 # Cell 5
@@ -60,78 +46,33 @@ inp, re = load(PATH+'train/100.png')
 # %%
 # Cell 6
 
-def resize(input_image, real_image, height, width):
-  input_image = tf.image.resize(input_image, [height, width],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-  real_image = tf.image.resize(real_image, [height, width],
-                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-  return input_image, real_image
-
-def random_crop(input_image, real_image):
-  stacked_image = tf.stack([input_image, real_image], axis=0)
-  cropped_image = tf.image.random_crop(
-      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
-
-  return cropped_image[0], cropped_image[1]
-
 # normalizing the images to [-1, 1]
-def normalize(input_image, real_image):
+def normalize(input_image):
   input_image = (input_image / 127.5) - 1
-  real_image = (real_image / 127.5) - 1
-
-  return input_image, real_image
-
-#@tf.function()
-def random_jitter(input_image, real_image):
-  # resizing to 286 x 286 x 3
-  input_image, real_image = resize(input_image, real_image, 286, 286)
-
-  # randomly cropping to 256 x 256 x 3
-  input_image, real_image = random_crop(input_image, real_image)
-
-  if tf.random.uniform(()) > 0.5:
-    # random mirroring
-    input_image = tf.image.flip_left_right(input_image)
-    real_image = tf.image.flip_left_right(real_image)
-
-  return input_image, real_image
+  return input_image
 
 # %%
 # Cell 8
 
-def load_image_train(image_file):
-  input_image, real_image = load(image_file)
-  input_image, real_image = random_jitter(input_image, real_image)
-  input_image, real_image = normalize(input_image, real_image)
-
-  return input_image, real_image
-
-def load_image_test(image_file):
-  input_image, real_image = load(image_file)
-  input_image, real_image = resize(input_image, real_image,
-                                   IMG_HEIGHT, IMG_WIDTH)
-  input_image, real_image = normalize(input_image, real_image)
-
-  return input_image, real_image
-
+def load_image(image_file):
+  input_image = load(image_file)
+  input_image = normalize(input_image)
+  return input_image
 # %% [markdown]
 # ## Input Pipeline
 
 # %%
 # Cell 9
 
-# Train
-train_dataset = tf.data.Dataset.list_files(PATH+'train/*.png')
-train_dataset = train_dataset.map(load_image_train,
+# T1
+T1_dataset = tf.data.Dataset.list_files(T1_PATH+'/*.bmp')
+T1_dataset = T1_dataset.map(load_image,
                                   num_parallel_calls=tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(BATCH_SIZE)
 
-# Test
-test_dataset = tf.data.Dataset.list_files(PATH+'test/*.png')
-test_dataset = test_dataset.map(load_image_test)
-test_dataset = test_dataset.batch(BATCH_SIZE)
+# T2
+T2_dataset = tf.data.Dataset.list_files(T2_PATH+'/*.bmp')
+T2_dataset = T2_dataset.map(load_image,
+                                  num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
 # %% [markdown]
 # ## Build the Generator
